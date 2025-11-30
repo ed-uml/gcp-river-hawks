cb115a65d4b37768bbe185fb6f8ffe78784e031c
ed-uml <edward_doyle@student.uml.edu>
2025-11-30 01:12:29 -0800

    .

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def hello_http(request):
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>River Hawk Cloud Deployment Week 6</title>
+        <title>River Hawk Cloud Deployment Week 6 GCP</title>
         
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
         
